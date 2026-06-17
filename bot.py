4b4a8c05eaea6cea56416dbf1da4816fbc2ba256
coodween123 <ga.andyul@gmail.com>
2026-06-17 16:09:35 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-BOT_TOKEN = "8956099107:AAHV7rykQEdon9w6dY_ANr7IuRsb5yx5Bvw"
+BOT_TOKEN = "ТОКЕН"
 DB_NAME = "design_kpi_bot.db"
 
 
